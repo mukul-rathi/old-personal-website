8f1dc9d3da421ea670c117d7e4deb5ba34c98a72
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-12 20:00:27 +0000

    Add stubs for home page components

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = ()=>{
+    return(
+        <footer>
+            <h2>Footer</h2>
+        </footer>
+
+    );
+
+
+}
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react"
 import NavBar from "./nav-bar";
+import Footer from './footer'
 
 const Layout = (props)=>{
     return(
         <div> 
-            <NavBar></NavBar>
+            <NavBar/>
             {props.children}
+            <Footer/>
         </div>
     );
 
