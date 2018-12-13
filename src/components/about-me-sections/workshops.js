f5ac9412dc3cea779acb416aace2aab349ecadb2
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-13 22:57:49 +0000

    Remove specific Workshop / Project cards and replace with Card component

--- src/components/about-me-sections/workshops.js
+++ src/components/about-me-sections/workshops.js
@@ -1,13 +1,49 @@
 import React from 'react'
-import Workshop from '../workshop';
+import Card from '../card';
 
 const Workshops = ()=>{
+    const IntroDeepLearn1 =  {
+        'title':"Intro to Neural Networks",
+        'img':{
+            'href':"https://www.youtube.com/watch?v=84iCkb7hqKs", 
+            'src':"",
+            'alt': "YouTube Link to Workshop Recording"
+        },
+        'button':{
+            'href':"https://hackersatcambridge.com/workshops/deep-learning",
+            'text': "Workshop Material"
+        }
+    };
+    const IntroDeepLearn2 =  {
+        'title':"Computer Vision",
+        'img':{
+            'href': "https://www.youtube.com/watch?v=bqPfxwrCBUk", 
+            'src':"",
+            'alt': "YouTube Link to Workshop Recording"
+        },
+        'button':{
+            'href':"https://hackersatcambridge.com/workshops/deep-learning",
+            'text': "Workshop Material"
+        }
+    };
+        const IntroDeepLearn3 =  {
+            'title':"Natural Language Processing",
+            'img':{
+                'href': "", 
+                'src':"",
+                'alt': "YouTube Link to Workshop Recording"
+            },
+            'button':{
+                'href':"https://hackersatcambridge.com/workshops/deep-learning",
+                'text': "Workshop Material"
+            }
+    };
     return(
         <div>
             <h2>Workshops</h2>
-            <Workshop title="Intro to Neural Networks" video="https://www.youtube.com/watch?v=84iCkb7hqKs" material="https://hackersatcambridge.com/workshops/deep-learning"> An Introduction to Neural Networks and how they learn. </Workshop>
-            <Workshop title="Computer Vision" video="https://www.youtube.com/watch?v=bqPfxwrCBUk" material="https://hackersatcambridge.com/workshops/deep-learning"> The theory behind Convolutional Neural Networks </Workshop>
-            <Workshop title="Natural Language Processing" video="" material="https://hackersatcambridge.com/workshops/deep-learning"> Recurrent Neural Networks </Workshop>
+            <Card title={IntroDeepLearn1.title} img = {IntroDeepLearn1.img} button={IntroDeepLearn1.button} > An Introduction to Neural Networks and how they learn. </Card>
+            <Card title={IntroDeepLearn2.title} img = {IntroDeepLearn2.img} button={IntroDeepLearn2.button} >The Theory Behind Convolutional Neural Networks </Card>
+            <Card title={IntroDeepLearn3.title} img = {IntroDeepLearn3.img} button={IntroDeepLearn3.button} >The Theory Behind Convolutional Neural Networks </Card>
         </div>
 
     );
--- src/components/homepage-sections/projects.js
+++ src/components/homepage-sections/projects.js
@@ -1,13 +1,52 @@
 import React from 'react'
-import Project from '../project';
+import Card from '../card';
 
 const Projects = ()=>{
+    const CambridgeGameGigHack = {
+        'title': "Retro Game Design" ,
+        'img': {
+            'href': "https://github.com/mukul-rathi/CambridgeGameGigHack",
+            'alt': "View on GitHub",
+            'src': ""
+        }
+    };
+    const OpenMined = {
+        'title': "OpenMined" ,
+        'img': {
+            'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
+            'alt': "View on GitHub",
+            'src': ""
+        }
+    };
+    const OxfordHack = {
+        'title': "OxfordHack - DeepDoc" ,
+        'img': {
+            'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
+            'alt': "View on GitHub",
+            'src': ""
+        }
+    };
     return(
         <div>
-            <h1>Projects</h1>
-            <Project title="Retro Game Design" srcCode="https://github.com/mukul-rathi/CambridgeGameGigHack">
+            <h1>Projects</h1> 
+            <Card img={CambridgeGameGigHack.img} title={CambridgeGameGigHack.title}>
                 This was a horizontal shooter game designed at the Cambridge GameGig 2017 Hackathon using Lua and the Love platform.  
-            </Project>
+            </Card>
+            <Card img={OpenMined.img} title={OpenMined.title}>
+                <p>
+                Contributed a demo notebook demonstrating the use of differential privacy, secure aggregation and federated learning using the Pysyft library. </p>
+                <p>
+                    This notebook's code was used by other contributors to generate more example usage.
+                </p>
+            </Card>
+            <Card img={OxfordHack.img} title={OxfordHack.title}>
+               <p>
+                   Built a dashboard to help doctors classify complicated diseases which aggregated patient feedback from a Facebook Messenger chatbot as well as displaying the predicted output.
+                </p>
+                <p>
+                    I used TensorFlow to train the neural network that used patient data to classify the type of cardiac arrhythmia.
+                </p>
+            </Card>
         </div>
 
     );
