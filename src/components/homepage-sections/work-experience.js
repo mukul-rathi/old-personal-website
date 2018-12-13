bd06ca90dc0cc28ac4c1e39373ddf30100f96305
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-13 23:57:12 +0000

    Update Work Experience section

--- src/components/homepage-sections/work-experience.js
+++ src/components/homepage-sections/work-experience.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Event from '../event';
 
 const WorkExperience = ()=>{
+    const JazzNetworks = {
+        'date' : "July - September 2018",
+        'location': "Jazz Networks",
+        'role': "Software Engineer Intern"
+    };
+    const CompVision = {
+        'date' : "July - August 2016",
+        'location': "Computer Vision Laboratory, University of Nottingham",
+        'role': "Deep Learning Research Intern"
+    };
     return(
         <div>
-            <h1>Work Experience</h1>
+            <h2>Experience</h2>
+            <Event date={JazzNetworks.date} location={JazzNetworks.location} role={JazzNetworks.role}>
+            Worked in the Machine Learning team using a beta-VAE for unsupervised clustering techniques.
+            </Event>
+            <Event date={CompVision.date} location={CompVision.location} role={CompVision.role}>
+            Used Fully Convolutional Networks for semantic segmentation.
+            </Event>
         </div>
 
     );
