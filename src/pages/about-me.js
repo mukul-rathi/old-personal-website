1bc91693b95e4921c377fb26a4e0ef3673619ecc
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-14 23:36:01 +0000

    Add CSS for transition image

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -6,11 +6,13 @@ import Workshops from "../components/about-me-sections/workshops";
 import TransitionImage from '../components/transition-image'
 import Hackathons from "../components/about-me-sections/hackathons";
 
+import transitionImageStyles from '../../css/transition-image.module.css'
+
 const AboutMe = () => {
     return (
         <Layout page="About Me">
             <WhoAmI/>
-            <TransitionImage text="Hackers At Cambridge"/>
+            <TransitionImage text="Hackers At Cambridge" className={transitionImageStyles.HaCImage}/>
             <Hackathons/>
             <Workshops/>
         </Layout>
