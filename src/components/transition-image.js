d4d786fbf00d3e9e9e3909eaf023c7d250379747
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-14 23:36:01 +0000

    Add CSS for transition image

--- src/components/transition-image.js
+++ src/components/transition-image.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import styles from '../../css/transition-image.module.css'
+import classNames from 'classnames'
 const TransitionImage = (props) =>{
     return(
-    <div>
+    <div className={classNames(styles.transitionImage, props.className)}>
         <h2>{props.text}</h2>
     </div>
     );
