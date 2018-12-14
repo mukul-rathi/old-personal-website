6c819af7c398af81699d44dca868daaf6d918e03
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-14 18:13:03 +0000

    Add css for hero image and basic shape of social button

--- src/components/homepage-sections/hero-image.js
+++ src/components/homepage-sections/hero-image.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import SocialButton from '../social-button'
-
+import styles from '../../../css/hero-image.module.css'
 
 const HeroImage = () =>{
     return(
-        <div className="hero-image">
-            <h1 className="hero-text"> Mukul Rathi</h1>
-            <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" alt="LinkedIn"/>
-            <SocialButton href="http://github.com/mukul-rathi" alt="GitHub"/>
+        <div className={styles.heroImage}>
+            <div className={styles.heroContent}>
+            <h1 className={styles.heroText}> Mukul Rathi</h1>
+            <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" alt="LinkedIn" className="linkedInButton"/>
+            <SocialButton href="http://github.com/mukul-rathi" alt="GitHub" className="githubButton"/>
+            </div>
         </div>
     );
 }
--- src/components/social-button.js
+++ src/components/social-button.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
+import classNames from 'classNames'
+import styles from '../../css/social-button.module.css'
 const SocialButton = (props) =>{
-
     return(
-        <a href={props.href}>
-        <img src={props.img} alt={props.alt}/>
+        <a href={props.href} className ={classNames(styles.socialButton, props.className)}>
         </a>
     );
 }
