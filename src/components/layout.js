ff2f1b72ae6efa104b85172cbea13e14679d90d9
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 11:46:28 +0000

    Remove default browser margin/padding

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
 import React from "react"
 import NavBar from "./nav-bar";
 import Footer from './footer'
-
+import styles from '../../css/layout.module.css'
 const Layout = (props)=>{
     return(
-        <div> 
+        <div className={styles.layout}> 
             <NavBar page = {props.page}/>
             {props.children}
             <Footer/>
