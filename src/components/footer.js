b136e0f42dc1d41a5f4816ebe9314ab8b8bbe618
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 14:27:31 +0000

    Add basic footer CSS

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SocialButton from './social-button';
-
+import styles from '../../css/footer.module.css'
 const Footer = ()=>{
     return(
         <footer>
+            <div>
             <SocialButton href="feed.xml" alt="RSS"/> <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" alt="LinkedIn"/>
             <SocialButton href="http://github.com/mukul-rathi" alt="GitHub"/>
+            </div>
         </footer>
 
     );
