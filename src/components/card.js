b96fce034bc5be32637125081748079bf8325109
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 13:39:11 +0000

    Add basic Material Design look to cards

--- src/components/card.js
+++ src/components/card.js
@@ -2,11 +2,13 @@ import React from 'react'
 import SocialButton from './social-button';
 import LinkButton from './link-button';
 import TextContent from './text-content';
+import styles from '../../css/card.module.css'
+
 
 const Card  = (props) => {
     //display social/link button only if defined
     return(
-        <div>
+        <div className={styles.card}>
             <h3> {props.title}</h3>
             {props.img && <SocialButton href={props.img.href} img={props.img.src} alt={props.img.alt}/> }
             
