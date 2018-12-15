f6ee8a25f71f02acd0a8fce5ae425e2bbebbb429
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 18:12:31 +0000

    Add card design CSS for education and experience

--- src/components/event.js
+++ src/components/event.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import TextContent from './text-content';
-
+import styles from '../../css/event.module.css'
 const Event = (props) =>{
     return(
-        <div>
-            <div>{props.date}
+        <div className={styles.event}>
+            <div className={styles.metaData}>
+                <h4 className={styles.date}>{props.date}</h4>
                 <img src={props.logo} alt="event-logo"></img>
             </div>
-            <div>
+            <div className={styles.mainContent}>
                 <h3> {props.location} </h3>
                 <h3> {props.role}</h3>
                 <TextContent>
