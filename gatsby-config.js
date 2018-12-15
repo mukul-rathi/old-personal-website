0dab344e935497a4fcbf7903afe8132a96dd7f42
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 20:25:56 +0000

    Add Gatsby Google Fonts Plugin

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: ['Oswald', 'Open Sans', 'Montserrat', 'Roboto', 'Raleway'
+        ]
+      }
+    }
+  ]
+}
