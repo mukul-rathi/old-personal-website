beb5305f372102291e7931e5d839ad5db42515bc
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 19:55:22 +0000

    Ensure consistency in CSS classes for each of the sections

--- src/components/about-me-sections/hackathons.js
+++ src/components/about-me-sections/hackathons.js
@@ -4,38 +4,35 @@ import styles from '../../../css/section.module.css'
 
 const Hackathons = ()=>{
     const gridProps = {
-        'sectionHeading' : "Hackathons",
-        'cards' : {
-            'GreenHack': {
-                'title':"GreenHack",
-                'img':{
-                    'href': "https://greenhack.hackersatcambridge.com/", 
-                    'src':"",
-                    'alt': "GreenHack Logo"
-                },
-                'button':{
-                    'href':"https://blogs.msdn.microsoft.com/uk_faculty_connection/2018/04/07/greenhack-iot-sustainability-hackathon-using-azure-services/",
-                    'text': "Microsoft Blog Post"
-                },
-                'description': "Led the publicity, updated the GreenHack website with project ideas and part of judging panel on the day."
+        'GreenHack': {
+            'title':"GreenHack",
+            'img':{
+                'href': "https://greenhack.hackersatcambridge.com/", 
+                'src':"",
+                'alt': "GreenHack Logo"
             },
+            'button':{
+                'href':"https://blogs.msdn.microsoft.com/uk_faculty_connection/2018/04/07/greenhack-iot-sustainability-hackathon-using-azure-services/",
+                'text': "Microsoft Blog Post"
+            },
+            'description': "Led the publicity, updated the GreenHack website with project ideas and part of judging panel on the day."
+        },
 
-            'GameGig': {
-                'title': "Cambridge Game Gig 2018",
-                'img':{
-                    'href': "https://game-gig-one-button.netlify.com/", 
-                    'src':"",
-                    'alt': "GameGig Logo"
-                },
-                'description':"Lead Organiser of GameGig: responsibilities including liasing with sponsors, booking venue and running and judging event on day"
-            }
+        'GameGig': {
+            'title': "Cambridge Game Gig 2018",
+            'img':{
+                'href': "https://game-gig-one-button.netlify.com/", 
+                'src':"",
+                'alt': "GameGig Logo"
+            },
+            'description':"Lead Organiser of GameGig: responsibilities including liasing with sponsors, booking venue and running and judging event on day"
         }
     };
 
     return(
-
         <div className={styles.section}>
-       {GridCards(gridProps)}
+            <h2>Hackathons</h2> 
+            {GridCards(gridProps)}
        </div>
     );
 
--- src/components/homepage-sections/projects.js
+++ src/components/homepage-sections/projects.js
@@ -4,41 +4,40 @@ import styles from '../../../css/section.module.css'
 
 const Projects = ()=>{
     const gridProps = {
-        'sectionHeading': 'Projects',
-        'cards' : {
-            'CambridgeGameGigHack' :{
-                'title': "Retro Game Design" ,
-                'img': {
-                    'href': "https://github.com/mukul-rathi/CambridgeGameGigHack",
-                    'alt': "View on GitHub",
-                    'src': ""
-                },
-                'description':  "This was a horizontal shooter game designed at the Cambridge GameGig 2017 Hackathon using Lua and the Love platform." 
+        'CambridgeGameGigHack' :{
+            'title': "Retro Game Design" ,
+            'img': {
+                'href': "https://github.com/mukul-rathi/CambridgeGameGigHack",
+                'alt': "View on GitHub",
+                'src': ""
+            },
+            'description':  "This was a horizontal shooter game designed at the Cambridge GameGig 2017 Hackathon using Lua and the Love platform." 
 
+        },
+        'OpenMined' : {
+            'title': "OpenMined" ,
+            'img': {
+                'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
+                'alt': "View on GitHub",
+                'src': ""
             },
-            'OpenMined' : {
-                'title': "OpenMined" ,
-                'img': {
-                    'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
-                    'alt': "View on GitHub",
-                    'src': ""
-                },
-                'description': "Contributed a demo notebook demonstrating the use of differential privacy, secure aggregation and federated learning using the Pysyft library. This notebook's code was used by other contributors to generate more example usage."
+            'description': "Contributed a demo notebook demonstrating the use of differential privacy, secure aggregation and federated learning using the Pysyft library. This notebook's code was used by other contributors to generate more example usage."
+        },
+        'OxfordHack' : {
+            'title': "OxfordHack - DeepDoc" ,
+            'img': {
+                'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
+                'alt': "View on GitHub",
+                'src': ""
             },
-            'OxfordHack' : {
-                'title': "OxfordHack - DeepDoc" ,
-                'img': {
-                    'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
-                    'alt': "View on GitHub",
-                    'src': ""
-                },
-                'description': " Built a dashboard to help doctors classify     complicated diseases which aggregated patient feedback from a Facebook Messenger chatbot as well as displaying the predicted output. I used TensorFlow to train the neural network that used patient data to classify the type of cardiac arrhythmia."
-            }
+            'description': " Built a dashboard to help doctors classify     complicated diseases which aggregated patient feedback from a Facebook Messenger chatbot as well as displaying the predicted output. I used TensorFlow to train the neural network that used patient data to classify the type of cardiac arrhythmia."
         }
+
     };
     return(
         <div className={styles.section}>
-       {GridCards(gridProps)}
+            <h2>Projects</h2> 
+            {GridCards(gridProps)}
        </div>
     );
 
