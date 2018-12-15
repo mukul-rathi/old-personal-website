6ab4bd7f7ee90cd3caac4d1cf7aa0735f8547344
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-15 14:19:02 +0000

    Refactor workshops/projects/hackathons to use standardised grid component for separation of content.

--- src/components/homepage-sections/projects.js
+++ src/components/homepage-sections/projects.js
@@ -1,57 +1,41 @@
-import React from 'react'
-import Card from '../card';
-import styles from '../../../css/grids.module.css'
+import GridCards from '../grid-cards';
 
 const Projects = ()=>{
-    const CambridgeGameGigHack = {
-        'title': "Retro Game Design" ,
-        'img': {
-            'href': "https://github.com/mukul-rathi/CambridgeGameGigHack",
-            'alt': "View on GitHub",
-            'src': ""
-        }
-    };
-    const OpenMined = {
-        'title': "OpenMined" ,
-        'img': {
-            'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
-            'alt': "View on GitHub",
-            'src': ""
-        }
-    };
-    const OxfordHack = {
-        'title': "OxfordHack - DeepDoc" ,
-        'img': {
-            'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
-            'alt': "View on GitHub",
-            'src': ""
+    const gridProps = {
+        'sectionHeading': 'Projects',
+        'cards' : {
+            'CambridgeGameGigHack' :{
+                'title': "Retro Game Design" ,
+                'img': {
+                    'href': "https://github.com/mukul-rathi/CambridgeGameGigHack",
+                    'alt': "View on GitHub",
+                    'src': ""
+                },
+                'description':  "This was a horizontal shooter game designed at the Cambridge GameGig 2017 Hackathon using Lua and the Love platform." 
+
+            },
+            'OpenMined' : {
+                'title': "OpenMined" ,
+                'img': {
+                    'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
+                    'alt': "View on GitHub",
+                    'src': ""
+                },
+                'description': "Contributed a demo notebook demonstrating the use of differential privacy, secure aggregation and federated learning using the Pysyft library. This notebook's code was used by other contributors to generate more example usage."
+            },
+            'OxfordHack' : {
+                'title': "OxfordHack - DeepDoc" ,
+                'img': {
+                    'href': "https://github.com/OpenMined/PySyft/blob/master/examples/torch/Boston_Housing_Federated_Training%20with%20Secure%20Aggregation%20and%20Diff%20Privacy.ipynb",
+                    'alt': "View on GitHub",
+                    'src': ""
+                },
+                'description': " Built a dashboard to help doctors classify     complicated diseases which aggregated patient feedback from a Facebook Messenger chatbot as well as displaying the predicted output. I used TensorFlow to train the neural network that used patient data to classify the type of cardiac arrhythmia."
+            }
         }
     };
     return(
-        <div className={styles.projects}>
-            <h1 className={styles.sectionHeading}>Projects</h1> 
-            <div className={styles.cards}>
-            <Card img={CambridgeGameGigHack.img} title={CambridgeGameGigHack.title}>
-                This was a horizontal shooter game designed at the Cambridge GameGig 2017 Hackathon using Lua and the Love platform.  
-            </Card>
-            <Card img={OpenMined.img} title={OpenMined.title}>
-                <p>
-                Contributed a demo notebook demonstrating the use of differential privacy, secure aggregation and federated learning using the Pysyft library. </p>
-                <p>
-                    This notebook's code was used by other contributors to generate more example usage.
-                </p>
-            </Card>
-            <Card img={OxfordHack.img} title={OxfordHack.title}>
-               <p>
-                   Built a dashboard to help doctors classify complicated diseases which aggregated patient feedback from a Facebook Messenger chatbot as well as displaying the predicted output.
-                </p>
-                <p>
-                    I used TensorFlow to train the neural network that used patient data to classify the type of cardiac arrhythmia.
-                </p>
-            </Card>
-            </div>
-        </div>
-
+       GridCards(gridProps)
     );
 
 
