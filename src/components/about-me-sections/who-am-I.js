d8901051614f0c7e538905f053a9352b28c02031
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-16 14:34:49 +0000

    Add profile picture to About Me section

--- src/components/about-me-sections/who-am-I.js
+++ src/components/about-me-sections/who-am-I.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import TextContent from '../text-content';
 import styles from '../../../css/who-am-I.module.css'
+import profilePic from '../../../assets/profile-pic.jpg'
 const WhoAmI = ()=>{
     return(
         <div className={styles.whoAmI}>
             <h1 className={styles.mainHeading}>About Me</h1>
-            <TextContent>
+            <img className={styles.profilePic} src={profilePic} alt="Mukul Rathi"/>
+            <div className={styles.description}>
             <p>
                 Hey there! Welcome to my personal website! A bit about me - I'm a second year Computer Science student at the University of Cambridge with an interest in all things deep learning! 
             </p>
@@ -16,8 +17,8 @@ const WhoAmI = ()=>{
                 In my free time at Cambridge, I am a core member of the <a href="https://hackersatcambridge.com">Hackers at Cambridge</a> committee - I am the Workshop Overseer and Events Overseer.
                 As Workshop Overseer, I organise and coordinate student-run workshops on different areas of computer science, some of which I present myself. As Events Overseer, I am responsible for leading the organisation of the weekly HaC nights, as well as the hackathons held at the end of each term.
             </p>
-            </TextContent>
-            <img className={styles.profilePic} alt="Mukul Rathi"/>
+            </div>
+           
         </div>
 
     );
