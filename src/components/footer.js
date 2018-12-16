7adfb7aa7873ca1654b776e0ea8bf7ecb6676ba2
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-16 20:09:30 +0000

    Add copyright to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import SocialButton from './social-button';
-//eslint-disable-next-line
 import styles from '../../css/footer.module.css'
 const Footer = ()=>{
     return(
         <footer>
-            <div>
+            <div className={styles.socialButton}>
             <SocialButton img='rss' href="feed.xml"/> <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" img="linkedIn"/>
             <SocialButton img='github' href="http://github.com/mukul-rathi" />
-            </div>
+            </div>      
+        <span className={styles.copyrightText}>© Mukul Rathi 2018 </span> 
         </footer>
 
     );
