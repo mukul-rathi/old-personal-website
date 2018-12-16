2ff237577dd7bdb7472a2e51d90290c429d79884
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-16 14:54:19 +0000

    Add event logos for education/experience

--- src/components/homepage-sections/education.js
+++ src/components/homepage-sections/education.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Event from '../event';
 import styles from '../../../css/section.module.css'
-
+import UniOfCam from '../../../assets/logos/uni-of-cam.png'
+import NottsHigh from '../../../assets/logos/notts-high.jpg'
 const Education = ()=>{
     const BatchDegree = {
         'date': "2017 - 2020",
@@ -16,12 +17,12 @@ const Education = ()=>{
     return(
         <div class={styles.section}>
             <h2>Education</h2>
-            <Event date={BatchDegree.date} location={BatchDegree.location} role={BatchDegree.educationLevel}>
+            <Event date={BatchDegree.date} location={BatchDegree.location} role={BatchDegree.educationLevel} logo={UniOfCam}>
             <ul>
             <li>First Year Results: Ranked 2nd out of 101 Computer Science students in my year. (First Class)</li>
             </ul>
             </Event>
-            <Event date={School.date} location={School.location} role={School.educationLevel}>
+            <Event date={School.date} location={School.location} role={School.educationLevel} logo={NottsHigh}>
             <ul>
             <li>A Levels: 4 A*s in Maths, Further Maths, Physics and Chemistry</li>
             <li>GCSEs: 11 A*s and A (top grade) in FSMQ Additional Maths</li>
