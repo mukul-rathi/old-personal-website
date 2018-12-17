33ff015694978f4f6dff373b72a1ed4285f9e747
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-17 13:12:11 +0000

    Redesign the card layout

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,56 @@
 import React from 'react'
 import SocialButton from './social-button';
 import LinkButton from './link-button';
-import TextContent from './text-content';
 import styles from '../../css/card.module.css'
-
+import OpenMined from '../../assets/hero-image.jpg'
 
 const Card  = (props) => {
     //display social/link button only if defined
+    
+    /*var props = {
+        'title': "",
+        'img': {
+            'src': "",
+            'alt': ""
+        },
+        'date': "",
+        'category': "",
+        'link1':{
+            'href': "",
+            'text' : ""
+        }, 
+        'link2':{
+            'href': "",
+            'text' : ""
+        },
+        'socialButton': {
+            'href': "",
+            'img': ""
+        }, 
+        'description': ""
+    };
+
+*/
     return(
-        <div className={styles.card}>
-            <h3> {props.title}</h3>
-            {props.icon &&<SocialButton img={props.icon.img} href={props.icon.href}/>}
-            {props.button &&  <LinkButton href={props.button.href}> {props.button.text} </LinkButton>}
-            
-            <TextContent>
-                {props.description}
-            </TextContent>
+    <div className={styles.card}>
+        <div className={styles.mainImage}>
+            { props.img && <img src={OpenMined} alt={props.img.alt}/>}
         </div>
+        <div className={styles.metaData}>
+            <h4 className={styles.category}>{props.category}</h4>
+            <h4 className={styles.date}>{props.date}</h4>
+        </div>
+        <h3 className={styles.title}>{props.title} </h3>
+        <div className={styles.description}>        {props.description}
+        </div>
+          <div className={styles.linkButtons}>
+          {props.link1 &&  <LinkButton href={props.link1.href}>{props.link1.text}</LinkButton>}
+            { props.link2 &&  <LinkButton href={props.link2.href}>{props.link2.text}</LinkButton>}
+          </div>
+          <div  className={styles.socialButton}>
+        { props.socialButton && SocialButton(props.socialButton) }
+         </div>
+     </div>
     );
 }
 
