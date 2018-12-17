53be183a3bb949cdbce1c255852e82bcb792402d
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-17 19:33:27 +0000

    Change fonts for cards and about me

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: ['Oswald', 'Roboto', 'Open Sans', 'Montserrat', 'Raleway', 'PT Sans'
+        fonts: ['Oswald', 'Roboto', 'Open Sans', 'Montserrat', 'Raleway', 'PT Sans', 'Lato'
         ]
       }
     }
