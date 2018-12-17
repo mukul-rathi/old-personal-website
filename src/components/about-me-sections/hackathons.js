11f0581b071bd13340b26b5088d165c0c00fdc00
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-17 17:53:44 +0000

    Add Hackathon Images

--- src/components/about-me-sections/hackathons.js
+++ src/components/about-me-sections/hackathons.js
@@ -2,30 +2,60 @@ import React from 'react'
 import GridCards from '../grid-cards';
 import styles from '../../../css/section.module.css'
 
+import GreenHackImg from '../../../assets/hackathons/greenhack.png'
+import GameGigImg from '../../../assets/hackathons/gamegig.png'
+
 const Hackathons = ()=>{
     const gridProps = {
         'GreenHack': {
-            'title':"GreenHack",
+            'title': "GreenHack",
             'img':{
-                'href': "https://greenhack.hackersatcambridge.com/", 
-                'src':"",
+                'src': GreenHackImg,
                 'alt': "GreenHack Logo"
             },
-            'button':{
-                'href':"https://blogs.msdn.microsoft.com/uk_faculty_connection/2018/04/07/greenhack-iot-sustainability-hackathon-using-azure-services/",
-                'text': "Microsoft Blog Post"
+            'date': "March 2018",
+            'category': "Sustainability",
+            'link1':{
+                'href': "https://greenhack.hackersatcambridge.com/",
+                'text' : "GreenHack Website"
             },
+            'link2':{
+                'href': "https://blogs.msdn.microsoft.com/uk_faculty_connection/2018/04/07/greenhack-iot-sustainability-hackathon-using-azure-services/",
+                'text' : "Microsoft Blog Post"
+            }, 
+           
+            'socialButton': {
+                'href':  "https://www.facebook.com/events/694391484282782/",
+                'img': "facebook"
+            }, 
             'description': "Led the publicity, updated the GreenHack website with project ideas and part of judging panel on the day."
         },
-
         'GameGig': {
             'title': "Cambridge Game Gig 2018",
             'button':{
                 'href': "https://game-gig-one-button.netlify.com/", 
                 'text': "GameGig Website"
             },
-            'description':"Lead Organiser of GameGig: responsibilities including liasing with sponsors, booking venue and running and judging event on day"
-        }
+            'description':""
+        },
+        'GameGig': {
+            'title': "Cambridge Game Gig 2018",
+            'img':{
+                'src': GameGigImg,
+                'alt': "Game Gig Logo"
+            },
+            'date': "December 2018",
+            'category': "Game Design",
+            'link1':{
+                'href': "https://hackersatcambridge.com/gamegig",
+                'text' : "GameGig Website"
+            },           
+            'socialButton': {
+                'href':  "https://www.facebook.com/events/https://www.facebook.com/events/1720940534684424//",
+                'img': "facebook"
+            }, 
+            'description': "Lead Organiser of GameGig: responsibilities including liasing with sponsors, booking venue and running and judging event on day"
+        },
     };
 
     return(
