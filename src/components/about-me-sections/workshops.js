6767b2b6840f19cd26cdcd1780cdc8d651b9232b
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-17 17:29:27 +0000

    Add workshops' images

--- src/components/about-me-sections/workshops.js
+++ src/components/about-me-sections/workshops.js
@@ -2,44 +2,55 @@ import React from 'react'
 import GridCards from '../grid-cards';
 import styles from '../../../css/section.module.css'
 
+import DeepLearn1 from '../../../assets/workshops/deep-learn-1.png'
+import DeepLearn2 from '../../../assets/workshops/deep-learn-2.png'
+
 const Workshops = ()=>{
     const gridProps = {
-        'IntroDeepLearn1': {
+        'IntroDeepLearn1':{
             'title':"Intro to Neural Networks",
-            'icon':{
+            'img': {
+                'src': DeepLearn1,
+                'alt': "Workshop Banner"
+            },
+            'date': "November 2018",
+            'category': "Deep Learning",
+            'link1':{
+                'href': "https://hackersatcambridge.com/workshops/deep-learning",
+                'text' : "View Workshop Material"
+            }, 
+            'link2':{
+                'href': "https://github.com/hackersatcambridge/workshop-deep-learning",
+                'text' : "GitHub Repo"
+            },
+            'socialButton': {
                 'href':"https://www.youtube.com/watch?v=84iCkb7hqKs", 
                 'img': "youtube"
-            },
-            'button':{
-                'href':"https://hackersatcambridge.com/workshops/deep-learning",
-                'text': "Workshop Material"
-            },
+            }, 
             'description': "An Introduction to Neural Networks and how they learn."
         },
-        'IntroDeepLearn2': {
-            'title':"Convolutional Neural Networks ",
-            'icon':{
-                'href': "https://www.youtube.com/watch?v=bqPfxwrCBUk", 
-                'img': 'youtube'
+        'IntroDeepLearn2':{
+            'title':"Convolutional Neural Networks",
+            'img': {
+                'src': DeepLearn2,
+                'alt': "Workshop Banner"
             },
-            'button':{
-                'href':"https://hackersatcambridge.com/workshops/deep-learning",
-                'text': "Workshop Material"
+            'date': "November 2018",
+            'category': "Deep Learning",
+            'link1':{
+                'href': "https://hackersatcambridge.com/workshops/deep-learning",
+                'text' : "View Workshop Material"
+            }, 
+            'link2':{
+                'href': "https://github.com/hackersatcambridge/workshop-deep-learning",
+                'text' : "GitHub Repo"
             },
+            'socialButton': {
+                'href':"https://www.youtube.com/watch?v=bqPfxwrCBUk", 
+                'img': "youtube"
+            }, 
             'description': "Deep learning for Computer Vision"
         },
-        'IntroDeepLearn3': {
-            'title':"Recurrent Neural Networks",
-            'icon':{
-                'href': "", 
-                'img': 'youtube'
-            },
-            'button':{
-                'href':"https://hackersatcambridge.com/workshops/deep-learning",
-                'text': "Workshop Material"
-            },
-            'description': "Applying Deep Learning to Natural Language Processing "
-        }
     }
     return(
         <div className={styles.section}>
