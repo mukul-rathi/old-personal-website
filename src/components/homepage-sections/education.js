36061776497eda4de86ab20008cce71bd362636c
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-17 07:50:35 +0000

    Add media queries to make event cards responsive

--- src/components/homepage-sections/education.js
+++ src/components/homepage-sections/education.js
@@ -5,9 +5,9 @@ import UniOfCam from '../../../assets/logos/uni-of-cam.png'
 import NottsHigh from '../../../assets/logos/notts-high.jpg'
 const Education = ()=>{
     const BatchDegree = {
-        'date': "2017 - 2020",
+        'date': "2017-2020",
         'location': "University of Cambridge",
-        'educationLevel': "B.A. (Hons) in Computer Science"
+        'educationLevel': "B.A. (Hons) Computer Science"
     }
     const School = {
         'date': '2010-2017',
@@ -15,18 +15,14 @@ const Education = ()=>{
         'educationLevel': "GCSEs and A Levels"
     }
     return(
-        <div class={styles.section}>
-            <h2>Education</h2>
+        <div className={styles.section}>
+            <h2 className={styles.sectionHeading}>Education</h2>
             <Event date={BatchDegree.date} location={BatchDegree.location} role={BatchDegree.educationLevel} logo={UniOfCam}>
-            <ul>
-            <li>First Year Results: Ranked 2nd out of 101 Computer Science students in my year. (First Class)</li>
-            </ul>
+            <strong>First Year Results:</strong> Ranked 2nd out of 101 Computer Science students in my year. (First Class)
             </Event>
             <Event date={School.date} location={School.location} role={School.educationLevel} logo={NottsHigh}>
-            <ul>
-            <li>A Levels: 4 A*s in Maths, Further Maths, Physics and Chemistry</li>
-            <li>GCSEs: 11 A*s and A (top grade) in FSMQ Additional Maths</li>
-            </ul>
+            <p><strong>A Levels:</strong> 4 A*s in Maths, Further Maths, Physics and Chemistry</p>
+            <p><strong>GCSEs:</strong> 11 A*s and A (top grade) in FSMQ Additional Maths</p>
             </Event>
         </div>
 
