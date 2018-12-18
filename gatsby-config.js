84f7f364576beb07118fafb4445a14a6de08a517
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-18 20:20:20 +0000

    Add blog posts to Gatsby Site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
     plugins: [
-    {
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `posts`,
+          path: `${__dirname}/posts/`,
+        },
+      },
+      `gatsby-transformer-remark`,
+      {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: ['Oswald', 'Roboto', 'Open Sans', 'Montserrat', 'Raleway', 'PT Sans', 'Lato'
