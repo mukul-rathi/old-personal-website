f4d69879a444ec121128a47cb3febd309ecf1076
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-18 21:12:01 +0000

    Add blog-post cards to blog homepage

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,45 @@
 import React from "react";
 import Layout from "../components/layout";
+import { graphql } from "gatsby"
+import BlogCard from "../components/blog-card";
 
-const Blog = () => {
+
+const Blog = ({data}) => {
     return (
         <Layout page="Blog">
             <h1>Blog </h1>
             <p> This is my blog stub. </p> 
+            {data.allMarkdownRemark.edges.map(({ node }, index) => (
+                BlogCard(node)
+            ))}
         </Layout>
     );
 }
 
 export default Blog;
+
+export const query = graphql`
+query{
+    allMarkdownRemark(
+        sort: {fields: [frontmatter___date], order: DESC}
+        ){
+        edges{
+        node{
+          fileAbsolutePath
+          frontmatter {
+            title
+            layout
+            comments
+            date
+            excerpt
+            _PARENT
+            series
+            part
+            image
+            caption
+          }
+        }
+      }
+    }
+  }
+`
