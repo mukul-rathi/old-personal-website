afde2d02e38de2d6b2f1721e936133be11c1b8af
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-20 21:03:49 +0000

    Fix #8 - Make Hero Image Responsive

--- src/components/homepage-sections/hero-image.js
+++ src/components/homepage-sections/hero-image.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import SocialButton from '../social-button'
 import styles from '../../../css/hero-image.module.css'
-
-const HeroImage = () =>{
-    return(
-        <div className={styles.heroImage}>
+import Img  from 'gatsby-image';
+import {StaticQuery, graphql} from 'gatsby'
+const HeroImage = () =>(
+    <StaticQuery
+    query = {graphql`{
+        allFile (filter: {name: {eq: "hero-image"}}){
+          edges{
+            node {
+                childImageSharp{
+                    fluid(maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+          }
+        }
+      }
+      `}
+      render= {data => (
+          <div className={styles.wrapper}>
+            <Img fluid={data.allFile.edges[0].node.childImageSharp.fluid} className={styles.heroImage}/>
+            <div className={styles.heroImageDiv}>
             <div className={styles.heroContent}>
-            <h1 className={styles.heroText}> Mukul Rathi</h1>
+             <h1 className={styles.heroText}> Mukul Rathi</h1>
+            
             <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" img="linkedIn"/>
             <SocialButton href="http://github.com/mukul-rathi" img="github"/>
             </div>
-        </div>
-    );
-}
+            </div>
+            </div>
+    )}/>
+)
 
 export default HeroImage;
+
