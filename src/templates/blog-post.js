1452a62c35a467974d8d09e698b7e4d4c0cf0b13
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-20 01:05:50 +0000

    Add disqus comments to blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,13 +3,20 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import styles from '../../css/blog-post.module.css'
 import { Helmet } from "react-helmet"
+import { DiscussionEmbed } from 'disqus-react';
 
 require("katex/dist/katex.min.css")
 require("prismjs/themes/prism.css")
 
  const BlogPost = ({ data }) => {
 
-  const post = data.markdownRemark    
+  const post = data.markdownRemark;
+  const disqusShortname = "https-mukul-rathi-github-io";
+  const disqusConfig = {
+    identifier: post.id,
+    title: post.frontmatter.title,
+  };
+
   return (
     <Layout>
         <Helmet>
@@ -19,7 +26,9 @@ require("prismjs/themes/prism.css")
       <div className={styles.blogPost}>
         <h1 className={styles.title}>{post.frontmatter.title}</h1>
         <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.html }} />
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </div>
+      
     </Layout>
   )
 }
@@ -30,12 +39,12 @@ export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      id
       frontmatter {
         title
         date
         series
         part
-        image
         caption
         excerpt
       }
