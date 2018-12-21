0a4f3d99ac41ffa179aa251c896c27c3473b9066
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-21 12:00:48 +0000

    Make nav bar fixed and slide up/down on scroll

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -2,24 +2,60 @@ import React from "react"
 import { Link } from "gatsby";
 import styles from '../../css/nav-bar.module.css'
 import classNames from 'classnames'
-const NavBar = (props)=>{
-    const navButtons = {
+
+
+class NavBar extends React.Component{
+    constructor(props){
+        super(props);
+        //bind value of this in function
+        this.handleScroll = this.handleScroll.bind(this);
+
+        this.state = {
+            prevYOffSet: 0,
+            hidden:false
+        };
+    }
+    handleScroll(){
+        let currentYOffSet = window.pageYOffset;
+
+        if(currentYOffSet<=this.state.prevYOffSet){
+            this.setState({hidden:false});
+        }
+        else{
+            if(currentYOffSet>100){
+            this.setState({hidden:true});
+            }
+        }
+        this.setState({prevYOffSet: currentYOffSet});
+    }
+
+    //Lifecycle methods
+    componentDidMount(){
+        document.addEventListener('scroll', this.handleScroll);
+    }
+    componentWillUnmount(){
+        document.removeEventListener('scroll', this.handleScroll);
+    }
+
+
+    navButtons = {
         "Home" : "/",
         "About Me" : "/about-me",
         "Blog" : "/blog"
     };
-   
-    return (
-        <nav className={styles.navBar}> 
-            {
-            Object.entries(navButtons).map(
-            ([key, value]) =>  (<Link 
-                className= {classNames(styles.navButton, (props.page===key)? styles.selectedNavButton : null)}
-                 to={value} key={key}> {key}</Link>)
-            )
-            }
-        </nav>
-    );
+   render(){
+        return (
+            <nav className={classNames(styles.navBar,(this.state.hidden)? styles.hiddenNavBar : null)} > 
+                {
+                Object.entries(this.navButtons).map(
+                ([key, value]) =>  (<Link 
+                    className= {classNames(styles.navButton, (this.props.page===key)? styles.selectedNavButton : null)}
+                    to={value} key={key}> {key}</Link>)
+                )
+                }
+            </nav>
+        );
+        }
 
 }
 
