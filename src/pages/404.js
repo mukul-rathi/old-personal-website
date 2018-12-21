08ca0c62e0512bed691607df560bbab72b41ca36
Mukul Rathi <mukul.rathi@gmail.com>
2018-12-21 01:08:12 +0000

    Add custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout";
+import styles from "../../css/404.module.css"
+
+export default () => {
+    return(
+    <Layout>
+        <div className={styles.content}>
+        <h1>This is not the page you are looking for. </h1>
+        <p>
+            Check out the blog to find the right blog post?
+        </p>
+        </div>
+    </Layout>
+    );
+}
