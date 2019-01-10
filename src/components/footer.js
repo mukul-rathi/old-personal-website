df8c36e51eeaacd925a91fe6beebd1ad9de8ec7b
Mukul Rathi <mukul.rathi@gmail.com>
2019-01-10 10:42:19 +0000

    Fix #17 - make footer icons smaller

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,11 +4,11 @@ import styles from '../../css/footer.module.scss'
 const Footer = ()=>{
     return(
         <footer>
-            <div className={styles.socialButton}>
-            <SocialButton img='rss' href="/rss.xml"/> <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" img="linkedIn"/>
-            <SocialButton img='github' href="http://github.com/mukul-rathi" />
+            <div>
+            <SocialButton img='rss' href="/rss.xml" className={styles.socialButton}/> <SocialButton href="https://www.linkedin.com/in/mukul-rathi-17230014a/" img="linkedIn" className={styles.socialButton}/>
+            <SocialButton img='github' href="http://github.com/mukul-rathi" className={styles.socialButton}/>
             </div>      
-        <span className={styles.copyrightText}>© Mukul Rathi 2018 </span> 
+        <div className={styles.copyrightText}>© Mukul Rathi 2018 </div> 
         </footer>
 
     );
