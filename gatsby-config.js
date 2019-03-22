dd49dcf60bfbc369577e8e7abfb714687bc640fe
Mukul Rathi <mukul.rathi@gmail.com>
2019-03-22 21:06:37 +0000

    Add Progressive Web App support
    Fix #27 - changed default favicon to temporary Queens' bridge favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,21 @@ module.exports = {
         }
       },
       {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: "Mukul's Personal Website and Blog",
+          short_name: "Mukul's Blog",
+          start_url: "/",
+          background_color: "#6b37bf",
+          theme_color: "#6b37bf",
+          // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+          // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+          display: "standalone",
+          icon: "assets/hero-image.jpg", // This path is relative to the root of the site.
+        },
+      },
+      `gatsby-plugin-offline`,
+      {
         resolve: `gatsby-plugin-feed`
       },
       {
