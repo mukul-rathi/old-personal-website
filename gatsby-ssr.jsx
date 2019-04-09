482f3ce715deebe6da89e41e20c739cd46a8b2cf
Mukul Rathi <mukul.rathi@gmail.com>
2019-04-09 21:55:13 +0100

    Prefetch Google Analytics

---
+++ gatsby-ssr.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      rel="dns-prefetch"
+      key="dns-prefetch-google-analytics"
+      href="https://www.google-analytics.com"
+    />
+  ]);
+};
+
+export default onRenderBody;
