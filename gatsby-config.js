9d32414a8fdd7cdf8cd2152fdef666adaa3c37f4
mukul-rathi <mukul.rathi@gmail.com>
2020-05-08 20:39:04 +0100

    Fix blurry images?

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,53 +2,55 @@ module.exports = {
   siteMetadata: {
     title: `Mukul's Blog`,
     description: `Mukul Rathi's personal website and blog`,
-    siteUrl: `https://mukulrathi.com`
+    siteUrl: `https://mukulrathi.com`,
   },
   plugins: [
-    `gatsby-plugin-sass`, {
+    `gatsby-plugin-sass`,
+    {
       resolve: `gatsby-plugin-purgecss`,
       options: {
-        printRejected:
-            true,       // Print removed selectors and processed file names
-        develop: true,  // Enable while using `gatsby develop`
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore
         // files/folders
-        purgeOnly: ['css/']  // Purge only these files/folders
-      }
+        purgeOnly: ["css/"], // Purge only these files/folders
+      },
     },
     {
       resolve: `gatsby-plugin-minify-classnames`,
       options: {
-        develop: true  // Enable on `gatsby develop`
-      }
+        develop: true, // Enable on `gatsby develop`
+      },
     },
-    `gatsby-transformer-sharp`, `gatsby-plugin-sharp`, {
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
       resolve: `gatsby-source-filesystem`,
-      options: {name: `posts`, path: `${__dirname}/posts/`}
+      options: { name: `posts`, path: `${__dirname}/posts/` },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: {name: `assets`, path: `${__dirname}/assets/`}
+      options: { name: `assets`, path: `${__dirname}/assets/` },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Mukul\'s Personal Website and Blog',
-        short_name: 'Mukul\'s Blog',
-        start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
+        name: "Mukul's Personal Website and Blog",
+        short_name: "Mukul's Blog",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
         // Enables "Add to Homescreen" prompt and disables browser UI (including
         // back button) see
         // https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon:
-            'assets/icon.png'  // This path is relative to the root of the site.
-      }
+        display: "standalone",
+        icon: "assets/icon.png", // This path is relative to the root of the site.
+      },
     },
-    `gatsby-plugin-offline`, {
+    `gatsby-plugin-offline`,
+    {
       resolve: `gatsby-plugin-feed-mdx`,
       options: {
         query: `
@@ -63,19 +65,20 @@ module.exports = {
             }
           }
         `,
-        feeds: [{
-          serialize: ({query: {site, allMdx}}) => {
-            return allMdx.edges.map(edge => {
-              return Object.assign({}, edge.node.frontmatter, {
-                description: edge.node.excerpt,
-                date: edge.node.frontmatter.datePublished,
-                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                custom_elements: [{'content:encoded': edge.node.html}]
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.datePublished,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                });
               });
-            });
-          },
-          query: `
+            },
+            query: `
               {
                 allMdx(
                   sort: { order: DESC, fields: [frontmatter___datePublished] },
@@ -94,10 +97,11 @@ module.exports = {
                 }
               }
             `,
-          output: '/rss.xml',
-          title: 'Mukul\'s Blog\'s RSS Feed'
-        }]
-      }
+            output: "/rss.xml",
+            title: "Mukul's Blog's RSS Feed",
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-gtag`,
@@ -105,10 +109,13 @@ module.exports = {
         // your google analytics tracking id
         trackingId: `UA-124576029-2`,
         // Puts tracking script in the head instead of the body
-        head: false
-      }
+        head: false,
+      },
     },
-    `gatsby-plugin-sitemap`, `gatsby-plugin-robots-txt`, {
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-robots-txt`,
+    `gatsby-remark-images`,
+    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
@@ -118,10 +125,12 @@ module.exports = {
             options: {
               // Add any KaTeX options from
               // https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`
-            }
+              strict: `ignore`,
+            },
           },
-          `gatsby-remark-autolink-headers`, `gatsby-remark-copy-linked-files`, {
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-copy-linked-files`,
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               // Class prefix for <pre> tags containing syntax highlighting;
@@ -131,7 +140,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
+              classPrefix: "language-",
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
@@ -156,68 +165,65 @@ module.exports = {
               // If setting this to true, the parser won't handle and highlight
               // inline
               // code used in markdown i.e. single backtick code like `this`.
-              noInlineHighlight: false
-            }
+              noInlineHighlight: false,
+            },
           },
           {
             resolve: `gatsby-remark-images`,
             options: {
-              showCaptions: ['title'],
+              showCaptions: ["title"],
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
 
-              maxWidth: 650
-            }
-          }
-        ]
-      }
+              maxWidth: 650,
+            },
+          },
+        ],
+      },
     },
-    `gatsby-plugin-react-helmet`, {
+    `gatsby-plugin-react-helmet`,
+    {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {fonts: [{family: `Lato`}, {family: `Open Sans`}]}
+      options: { fonts: [{ family: `Lato` }, { family: `Open Sans` }] },
     },
     {
       resolve: `gatsby-plugin-google-amp`,
       options: {
         analytics: {
-          type: 'gtag',
-          dataCredentials: 'include',
+          type: "gtag",
+          dataCredentials: "include",
           config: {
             vars: {
-              gtag_id: 'UA-124576029-2',
-              config: {'UA-124576029-2': {page_location: '{{pathname}}'}}
-            }
-          }
+              gtag_id: "UA-124576029-2",
+              config: { "UA-124576029-2": { page_location: "{{pathname}}" } },
+            },
+          },
         },
-        canonicalBaseUrl: 'https://mukulrathi.com',
-        components: ['amp-form'],
-        excludedPaths: ['/404*', '/'],
-        pathIdentifier: '/amp/',
-        relAmpHtmlPattern: '{{canonicalBaseUrl}}{{pathname}}{{pathIdentifier}}',
-        useAmpClientIdApi: true
-      }
+        canonicalBaseUrl: "https://mukulrathi.com",
+        components: ["amp-form"],
+        excludedPaths: ["/404*", "/"],
+        pathIdentifier: "/amp/",
+        relAmpHtmlPattern: "{{canonicalBaseUrl}}{{pathname}}{{pathIdentifier}}",
+        useAmpClientIdApi: true,
+      },
     },
     // make sure to put last in the array
     {
       resolve: `gatsby-plugin-netlify`,
       options: {
-        headers: {},  // option to add more headers. `Link` headers are
+        headers: {}, // option to add more headers. `Link` headers are
         // transformed by the below criteria
         allPageHeaders: [
-          'Link: https://www.google-analytics.com; rel=preload; as=script'
-        ],  // option to add headers for all pages. `Link` headers are
+          "Link: https://www.google-analytics.com; rel=preload; as=script",
+        ], // option to add headers for all pages. `Link` headers are
         // transformed by the below criteria
-        mergeSecurityHeaders:
-            true,  // boolean to turn off the default security headers
-        mergeLinkHeaders:
-            true,  // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders:
-            true,  // boolean to turn off the default caching headers
-        generateMatchPathRewrites:
-            true  // boolean to turn off automatic creation of redirect rules
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules
         // for client only paths
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
